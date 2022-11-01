d9d27bc7b05663a27ccf7a19b3fcad0025c91645
Anton Zarichny <antonzarichny@gmail.com>
2022-11-01 14:10:08 +0300

    Initial Commit

---
+++ ContactsSUI/ContactsSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsSUIApp.swift
+//  ContactsSUI
+//
+//  Created by Антон Заричный on 01.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactsSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
